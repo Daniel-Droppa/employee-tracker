fe34aaf024a138132c40c27b0dbe55eda667e7c8
Daniel Droppa <danwdroppa@gmail.com>
2020-03-08 22:04:43 -0400

    added switch case for inquirer

--- employee_db.sql
+++ employee_db.sql
@@ -2,13 +2,46 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE employees (
+
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  departemnt VARCHAR(100) NOT NULL,
-  salary INT default 0,
-  manager VARCHAR(100) NOT NULL,
+  dept_name VARCHAR(30),
+PRIMARY KEY (id)
+);
+CREATE TABLE roll (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
   PRIMARY KEY (id)
 );
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30)
+  roll_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
+
+
+-- CREATE TABLE employees (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   first_name VARCHAR(100) NOT NULL,
+--   last_name VARCHAR(100) NOT NULL,
+--   title VARCHAR(100) NOT NULL,
+--   departemnt VARCHAR(100) NOT NULL,
+--   salary INT default 0,
+--   manager VARCHAR(100) NULL,
+--   PRIMARY KEY (id)
+-- );
+
+-- INSERT INTO employees (first_name, last_name, title, departemnt, salary, manager)
+-- VALUES ("John", "Doe", "Sales Lead", "Sales", 100000, "Ashley Rodriguez");
+
+-- INSERT INTO employees (first_name, last_name, title, departemnt, salary, manager)
+-- VALUES ("Mike","Chan", "Sales Person", "Sales", 80000, "John Doe");
+
+-- INSERT INTO employees (first_name, last_name, title, departemnt, salary, manager)
+-- VALUES ("Ashley", "Rodriguez", "Lead Engeneer", "Engeneering", 150000, null);
--- server.js
+++ server.js
@@ -1,2 +1,76 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer")
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "rootroot",
+    database: "employee_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    init()
+    
+});
+const init = function() {
+    inquirer
+      .prompt({
+        type: "list",
+        name: "startQ",
+        message: "What would you like to do?",
+        choices: [
+          "view all employees",
+          "view all roles",
+          "view all departments",
+          "add employee",
+          "add department",
+          "add role",
+          "update employee role",
+          "remove employee"
+        ]
+      })
+      .then(function(answer) {
+        console.log(answer);
+        // start of switch statment for user choice
+        switch (answer.startQ) {
+          case "view all employees":
+            viewallemployees();
+            break;
+  
+          case "view all roles":
+            viewallroles();
+            break;
+  
+          case "view all departments":
+            viewalldepartments();
+            break;
+  
+          case "add employee":
+            addEmployee();
+            break;
+  
+          case "update employee role":
+            updateEmpRole();
+            break;
+  
+          case "add department":
+            addDepartment();
+            break;
+  
+          case "add role":
+            addRole();
+            break;
+        }
+      });
+  };
+  init();
+
