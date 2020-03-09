843088607f5f99942f562b6abd23cd1420ea8fa4
Daniel Droppa <danwdroppa@gmail.com>
2020-03-09 00:00:31 -0400

    updated server.js

--- employee_db.sql
+++ employee_db.sql
@@ -1,47 +1,44 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db;
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+USE employee_DB;
 
-
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  dept_name VARCHAR(30),
-PRIMARY KEY (id)
-);
-CREATE TABLE roll (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
-  PRIMARY KEY (id)
-);
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30)
-  roll_id INT,
-  manager_id INT,
-  PRIMARY KEY (id)
+CREATE TABLE department(
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE roles(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+title VARCHAR (30),
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
+CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+roles_id INT NOT NULL,
+CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id),
+manager_id INT, 
+CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+SELECT * FROM department;
+SELECT * FROM roles;
+SELECT * FROM employee;
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("VP Sales", 100000, 1),
+         ("Salesperson", 80000, 1), 
+         ("Lead Engineer", 150000, 2), 
+         ("Software Engineer", 120000, 2),
+         ("Accountant", 125000, 3),
+         ("Lead Lawyer", 190000, 4), 
+         ("Lawyer", 130000, 4); 
 
 
--- CREATE TABLE employees (
---   id INT NOT NULL AUTO_INCREMENT,
---   first_name VARCHAR(100) NOT NULL,
---   last_name VARCHAR(100) NOT NULL,
---   title VARCHAR(100) NOT NULL,
---   departemnt VARCHAR(100) NOT NULL,
---   salary INT default 0,
---   manager VARCHAR(100) NULL,
---   PRIMARY KEY (id)
--- );
-
--- INSERT INTO employees (first_name, last_name, title, departemnt, salary, manager)
--- VALUES ("John", "Doe", "Sales Lead", "Sales", 100000, "Ashley Rodriguez");
-
--- INSERT INTO employees (first_name, last_name, title, departemnt, salary, manager)
--- VALUES ("Mike","Chan", "Sales Person", "Sales", 80000, "John Doe");
-
--- INSERT INTO employees (first_name, last_name, title, departemnt, salary, manager)
--- VALUES ("Ashley", "Rodriguez", "Lead Engeneer", "Engeneering", 150000, null);
